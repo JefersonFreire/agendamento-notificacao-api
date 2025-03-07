dbd0757218d9d7a194ad32c6a159e8ae46283cdd
JefersonFreire <jfreire86@hotmail.com>
2025-03-07 11:35:02 -0300

    adicionado meodos de cancelamento de notificações

--- src/main/java/com/jeferson/agendamento_notificacao_api/business/AgendamentoService.java
+++ src/main/java/com/jeferson/agendamento_notificacao_api/business/AgendamentoService.java
@@ -3,9 +3,11 @@ package com.jeferson.agendamento_notificacao_api.business;
 import com.jeferson.agendamento_notificacao_api.business.mapper.IAgendamentoMapper;
 import com.jeferson.agendamento_notificacao_api.controller.dto.in.AgendamentoRequestDTO;
 import com.jeferson.agendamento_notificacao_api.controller.dto.out.AgendamentoResponseDTO;
+import com.jeferson.agendamento_notificacao_api.infrastructure.entities.Agendamento;
 import com.jeferson.agendamento_notificacao_api.infrastructure.exception.NotFoundException;
 import com.jeferson.agendamento_notificacao_api.infrastructure.repositories.AgendamentoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AgendamentoService {
@@ -29,5 +31,12 @@ public class AgendamentoService {
                 .orElseThrow(() -> new NotFoundException("Id não encontrado")));
     }
 
+    public void cancelarAgendamento(Long id){
+        Agendamento agendamento = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Id não encontrado"));
+        repository.save(
+                agendamentoMapper.paraEntityCancelamento(agendamento));
+
+    }
 
 }
--- src/main/java/com/jeferson/agendamento_notificacao_api/business/mapper/IAgendamentoMapper.java
+++ src/main/java/com/jeferson/agendamento_notificacao_api/business/mapper/IAgendamentoMapper.java
@@ -4,6 +4,8 @@ import com.jeferson.agendamento_notificacao_api.controller.dto.in.AgendamentoReq
 import com.jeferson.agendamento_notificacao_api.controller.dto.out.AgendamentoResponseDTO;
 import com.jeferson.agendamento_notificacao_api.infrastructure.entities.Agendamento;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -13,4 +15,8 @@ public interface IAgendamentoMapper {
     Agendamento paraEntity(AgendamentoRequestDTO agendamento);
 
     AgendamentoResponseDTO paraDTO(Agendamento agendamento);
+
+    @Mapping(target = "dataHoraModificacao", expression = "java(LocalDateTime.now())")
+    @Mapping(target = "statusNotificacao", expression = "java(StatusNotificacaoEnum.CANCELADO)")
+    Agendamento paraEntityCancelamento(Agendamento agendamento);
 }
